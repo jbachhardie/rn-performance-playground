0c2aad3bda9f008e9442d484b457d6ae25f8aeb4
Jae Bach <jae.bach@wealthsimple.com>
2022-11-29 16:19:49 -0500

    Add benchmarks

--- App.js
+++ App.js
@@ -1,27 +1,350 @@
+import { useState, useMemo, useCallback, memo } from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import { NavigationContainer } from "@react-navigation/native";
-import { Button, View } from "react-native";
+import { NavigationContainer, useIsFocused } from "@react-navigation/native";
+import { useEffect } from "react";
+import { Button, View, Text, Animated, Easing, StyleSheet } from "react-native";
 
-function HomeScreen({ navigation }) {
+function slowFn(baseNumber) {
+  let result = 0;
+  for (var i = Math.pow(baseNumber, 7); i >= 0; i--) {
+    result += Math.atan(i) * Math.tan(i);
+  }
+}
+
+const DURATION = 3000;
+
+function NativeTestView() {
+  const hue = useMemo(() => new Animated.Value(0), []);
+  useEffect(() => {
+    const animation = Animated.loop(
+      Animated.timing(hue, {
+        toValue: 1,
+        duration: DURATION,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      })
+    );
+    animation.start();
+    return () => animation.stop();
+  }, [hue]);
+  const translateX = hue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [-100, 100],
+  });
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button onPress={navigation.openDrawer} title="Open navigation drawer" />
-      <Button
-        onPress={() => navigation.navigate("Notifications")}
-        title="Go to notifications"
-      />
+    <Animated.View
+      style={{
+        height: 100,
+        width: 100,
+        backgroundColor: "red",
+        transform: [{ translateX }],
+      }}
+    >
+      <Text>Native animation</Text>
+    </Animated.View>
+  );
+}
+
+function useTestView() {
+  const [animated, setAnimated] = useState(0);
+  const startTime = useMemo(() => Date.now(), []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setAnimated(((Date.now() - startTime) / DURATION) % 1);
+    }, 16.66);
+    return () => clearInterval(interval);
+  }, [setAnimated]);
+
+  return {
+    height: 100,
+    width: 100,
+    backgroundColor: "cyan",
+    transform: [{ translateX: animated * 200 - 100 }],
+  };
+}
+
+const viewStyle = StyleSheet.create({
+  flex: 1,
+  alignItems: "center",
+  justifyContent: "center",
+  marginTop: 80,
+});
+
+function HomeScreen() {
+  const testView = useTestView();
+
+  return (
+    <View style={viewStyle}>
+      <View style={testView}>
+        <Text>JS Render loop</Text>
+      </View>
+      <NativeTestView />
+      <Text style={{ margin: 50 }}>
+        The above squares updates at up to 60fps using React's render loop. The
+        different screens of this app introduce various different patterns to
+        see how they affect performance of that animation. The distance between
+        the squares is proportional to the initial render lag, while jitteriness
+        is proportional to the contention in the respective thread.
+      </Text>
     </View>
   );
 }
 
-function NotificationsScreen({ navigation }) {
+function SlowComponentScreen() {
+  const testView = useTestView();
+  slowFn(15);
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button onPress={navigation.openDrawer} title="Open navigation drawer" />
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    <View style={viewStyle}>
+      <View style={testView}>
+        <Text>JS Render loop</Text>
+      </View>
+      <NativeTestView />
+      <Text style={{ margin: 50 }}>
+        Here, the component body is doing something that requires a lot of CPU
+        work, slowing down the JS thread but not the UI thread.
+      </Text>
     </View>
   );
 }
+
+function ManyScreen() {
+  const testView = useTestView();
+  const a = [...Array(2000).keys()];
+
+  return (
+    <View style={viewStyle}>
+      <View style={testView}>
+        <Text>JS Render loop</Text>
+      </View>
+      <NativeTestView />
+      <Text style={{ margin: 50 }}>
+        Here we are rendering 2000 Text elements, slowing down the JS thread.
+      </Text>
+      <View style={{ flex: 1, flexDirection: "row", flexWrap: "wrap" }}>
+        {a.map((i) => (
+          <Text key={i}>{i}</Text>
+        ))}
+      </View>
+    </View>
+  );
+}
+
+const _ManyMemo = memo(() => {
+  const a = [...Array(2000).keys()];
+  return (
+    <View style={{ flex: 1, flexDirection: "row", flexWrap: "wrap" }}>
+      {a.map((i) => (
+        <Text key={i}>{i}</Text>
+      ))}
+    </View>
+  );
+});
+
+function ManyMemoScreen() {
+  const testView = useTestView();
+  const a = [...Array(2000).keys()];
+
+  return (
+    <View style={viewStyle}>
+      <View style={testView}>
+        <Text>JS Render loop</Text>
+      </View>
+      <NativeTestView />
+      <Text style={{ margin: 50 }}>
+        Here we are rendering 2000 Text elements but they are wrapped in
+        React.memo making it so they do not have to recalculate on update.
+      </Text>
+      <_ManyMemo />
+    </View>
+  );
+}
+
+function ManyWithUnstableKeysScreen() {
+  const testView = useTestView();
+  const a = [...Array(45).keys()];
+
+  return (
+    <View style={viewStyle}>
+      <View style={testView}>
+        <Text>JS Render loop</Text>
+      </View>
+      <NativeTestView />
+      <Text style={{ margin: 50 }}>
+        Here we are rendering 45 elements with unstable keys, forcing the
+        reconciler to update and slowing down the UI thread.
+      </Text>
+      <View style={{ flex: 1, flexDirection: "row", flexWrap: "wrap" }}>
+        {a.map((i) => (
+          <Text key={Math.random()}>{i}</Text>
+        ))}
+      </View>
+    </View>
+  );
+}
+
+function ManyWithCallbackScreen() {
+  const testView = useTestView();
+  const a = [...Array(100).keys()];
+  const onPress = () => console.log("boop!");
+
+  return (
+    <View style={viewStyle}>
+      <View style={testView}>
+        <Text>JS Render loop</Text>
+      </View>
+      <NativeTestView />
+      <Text style={{ margin: 50 }}>
+        Here we are rendering a very large number of elements with an unmemoized
+        callback.
+      </Text>
+      <View style={{ flex: 1, flexDirection: "row", flexWrap: "wrap" }}>
+        {a.map((i) => (
+          <Button onPress={onPress} key={i} title={i.toString()}>
+            {i}
+          </Button>
+        ))}
+      </View>
+    </View>
+  );
+}
+
+function ManyWithCallbackMemoizedScreen() {
+  const testView = useTestView();
+  const a = [...Array(100).keys()];
+  const onPress = useCallback(() => console.log("boop!"), []);
+
+  return (
+    <View style={viewStyle}>
+      <View style={testView}>
+        <Text>JS Render loop</Text>
+      </View>
+      <NativeTestView />
+      <Text style={{ margin: 50 }}>
+        Here we are rendering a very large number of elements with a memoized
+        callback.
+      </Text>
+      <View style={{ flex: 1, flexDirection: "row", flexWrap: "wrap" }}>
+        {a.map((i) => (
+          <Button onPress={onPress} key={i} title={i.toString()}>
+            {i}
+          </Button>
+        ))}
+      </View>
+    </View>
+  );
+}
+
+const onPress = () => console.log("boop!");
+function ManyWithCallbackStaticScreen() {
+  const testView = useTestView();
+  const a = [...Array(100).keys()];
+
+  return (
+    <View style={viewStyle}>
+      <View style={testView}>
+        <Text>JS Render loop</Text>
+      </View>
+      <NativeTestView />
+      <Text style={{ margin: 50 }}>
+        Here we are rendering a very large number of elements with a memoized
+        callback.
+      </Text>
+      <View style={{ flex: 1, flexDirection: "row", flexWrap: "wrap" }}>
+        {a.map((i) => (
+          <Button onPress={onPress} key={i} title={i.toString()}>
+            {i}
+          </Button>
+        ))}
+      </View>
+    </View>
+  );
+}
+
+function ManyWithStylesScreen() {
+  const testView = useTestView();
+  const a = [...Array(2000).keys()];
+  const style = { fontWeight: "bold" };
+
+  return (
+    <View style={viewStyle}>
+      <View style={testView}>
+        <Text>JS Render loop</Text>
+      </View>
+      <NativeTestView />
+      <Text style={{ margin: 50 }}>
+        Here we are rendering a very large number of elements with an unmemoized
+        styles object.
+      </Text>
+      <View style={{ flex: 1, flexDirection: "row", flexWrap: "wrap" }}>
+        {a.map((i) => (
+          <Text style={style} key={i}>
+            {i}
+          </Text>
+        ))}
+      </View>
+    </View>
+  );
+}
+
+function ManyWithStylesMemoizedScreen() {
+  const testView = useTestView();
+  const a = [...Array(2000).keys()];
+  const style = useMemo(() => ({ fontWeight: "bold" }), []);
+
+  return (
+    <View style={viewStyle}>
+      <View style={testView}>
+        <Text>JS Render loop</Text>
+      </View>
+      <NativeTestView />
+      <Text style={{ margin: 50 }}>
+        Here we are rendering a very large number of elements with a memoized
+        styles object.
+      </Text>
+      <View style={{ flex: 1, flexDirection: "row", flexWrap: "wrap" }}>
+        {a.map((i) => (
+          <Text style={style} key={i}>
+            {i}
+          </Text>
+        ))}
+      </View>
+    </View>
+  );
+}
+
+const style = { fontWeight: "bold" };
+function ManyWithStylesStaticScreen() {
+  const testView = useTestView();
+  const a = [...Array(2000).keys()];
+
+  return (
+    <View style={viewStyle}>
+      <View style={testView}>
+        <Text>JS Render loop</Text>
+      </View>
+      <NativeTestView />
+      <Text style={{ margin: 50 }}>
+        Here we are rendering a very large number of elements with a memoized
+        styles object.
+      </Text>
+      <View style={{ flex: 1, flexDirection: "row", flexWrap: "wrap" }}>
+        {a.map((i) => (
+          <Text style={style} key={i}>
+            {i}
+          </Text>
+        ))}
+      </View>
+    </View>
+  );
+}
+
+const withUnmounting = (Comp) => () => {
+  const isFocused = useIsFocused();
+
+  return isFocused ? <Comp /> : null;
+};
 
 const Drawer = createDrawerNavigator();
 
@@ -29,8 +352,44 @@ export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        <Drawer.Screen name="Home" component={withUnmounting(HomeScreen)} />
+        <Drawer.Screen
+          name="SlowComponent"
+          component={withUnmounting(SlowComponentScreen)}
+        />
+        <Drawer.Screen name="Many" component={withUnmounting(ManyScreen)} />
+        <Drawer.Screen
+          name="ManyMemo"
+          component={withUnmounting(ManyMemoScreen)}
+        />
+        <Drawer.Screen
+          name="ManyWithUnstableKeys"
+          component={withUnmounting(ManyWithUnstableKeysScreen)}
+        />
+        <Drawer.Screen
+          name="ManyWithCallback"
+          component={withUnmounting(ManyWithCallbackScreen)}
+        />
+        <Drawer.Screen
+          name="ManyWithCallbackMemoized"
+          component={withUnmounting(ManyWithCallbackMemoizedScreen)}
+        />
+        <Drawer.Screen
+          name="ManyWithCallbackStatic"
+          component={withUnmounting(ManyWithCallbackStaticScreen)}
+        />
+        <Drawer.Screen
+          name="ManyWithStyles"
+          component={withUnmounting(ManyWithStylesScreen)}
+        />
+        <Drawer.Screen
+          name="ManyWithStylesMemoized"
+          component={withUnmounting(ManyWithStylesMemoizedScreen)}
+        />
+        <Drawer.Screen
+          name="ManyWithStylesStatic"
+          component={withUnmounting(ManyWithStylesStaticScreen)}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
--- README.md
+++ README.md
@@ -1,21 +1,11 @@
-# Drawer navigation example
+# React Native Performance Tests
 
-<p>
-  <!-- iOS -->
-  <img alt="Supports Expo iOS" longdesc="Supports Expo iOS" src="https://img.shields.io/badge/iOS-4630EB.svg?style=flat-square&logo=APPLE&labelColor=999999&logoColor=fff" />
-  <!-- Android -->
-  <img alt="Supports Expo Android" longdesc="Supports Expo Android" src="https://img.shields.io/badge/Android-4630EB.svg?style=flat-square&logo=ANDROID&labelColor=A4C639&logoColor=fff" />
-  <!-- Web -->
-  <img alt="Supports Expo Web" longdesc="Supports Expo Web" src="https://img.shields.io/badge/web-4630EB.svg?style=flat-square&logo=GOOGLE-CHROME&labelColor=4285F4&logoColor=fff" />
-</p>
+This repo sets out to test what can affect React Native rendering performance,
+focusing on the issue of memoization and unstable references. This is not
+intended as a comprehensive benchmark but rather a visual exploration of the
+real-world jank that might be expected from different approaches.
 
 ## 🚀 How to use
 
 - Install with `yarn` or `npm install`.
 - Run `yarn start` or `npm run start` to try it out.
-
-## 📝 Notes
-
-- This is a very basic example from the [react navigation](https://reactnavigation.org/) docs.
-- [Drawer navigation documentation](https://reactnavigation.org/docs/drawer-based-navigation).
-- [Reanimated Babel plugin may be required](https://github.com/software-mansion/react-native-reanimated/issues/3410)
